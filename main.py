71da058df449ab47ee0b6913d1ff3c522c84d0b0
zhiruisong02 <zjs5301@psu.edu>
2020-09-18 15:02:28 +0000

    1

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ def digit_sum(n):
   if n<10:
     return n
   else:
-    r = n//10
-    m = n%10
-    return m+digit_sum(r)
+    return n%10+digit_sum(n//10)
 
 def run():
   n = input ("Enter an int: ")
